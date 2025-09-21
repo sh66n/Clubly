ec73bf0bc6ac4fe1e23f41951e4953b59dc4f614
sh66n <shaanedroos85@gmail.com>
2025-09-21 18:50:36 +0530

    group attendance frontend

--- src/app/(app)/events/[eventId]/attendance/page.tsx
+++ src/app/(app)/events/[eventId]/attendance/page.tsx
@@ -1,14 +1,18 @@
 import AttendanceMarker from "@/components/Events/AttendanceMarker";
 import UserCard from "@/components/Events/AttendanceUserCard";
+import { connectToDb } from "@/lib/connectToDb";
+import { Event } from "@/models";
 import { getAttendance } from "@/services/getAttendance";
 import { getParticipants } from "@/services/getParticipants";
 import { getPendingRegistrations } from "@/services/getPendingRegistrations";
 import React from "react";
 
-const getData = async (eventId) => {
-  const participants = await getParticipants(eventId);
-  const pendingRegistrations = await getPendingRegistrations(eventId);
-  return { participants, pendingRegistrations };
+const getEvent = async (eventId: string) => {
+  await connectToDb();
+  const event = await Event.findById(eventId).select("eventType");
+  if (!event) throw new Error("Event not found");
+
+  return event.eventType;
 };
 
 export default async function Attendance({
@@ -18,24 +22,25 @@ export default async function Attendance({
 }) {
   const { eventId } = await params;
   const attendanceData = await getAttendance(eventId);
+  const eventType = await getEvent(eventId);
 
-  const safeAttendanceData = attendanceData.map((u) => ({
-    ...u,
-    _id: u._id.toString(), // convert ObjectId → string
+  const safeAttendanceData = attendanceData.map((entrant) => ({
+    ...entrant,
+    _id: entrant._id.toString(), // convert ObjectId → string
   }));
 
-  const present = safeAttendanceData.filter((user) => user.present);
-  const absent = safeAttendanceData.filter((user) => !user.present);
+  const present = safeAttendanceData.filter((entrant) => entrant.present);
+  const absent = safeAttendanceData.filter((entrant) => !entrant.present);
 
   return (
     <div className="h-full flex flex-col">
       <div className="text-3xl mb-4">Attendance</div>
-      <AttendanceMarker present={present} absent={absent} eventId={eventId} />
-      {/* <div>
-        {pendingRegistrations.map((user, i) => (
-          <UserCard key={user._id} user={user} />
-        ))}
-      </div> */}
+      <AttendanceMarker
+        present={present}
+        absent={absent}
+        eventId={eventId}
+        eventType={eventType} // pass event type
+      />
     </div>
   );
 }
