632c5bdc86b012386283c9ae187546e7e533433b
sh66n <shaanedroos85@gmail.com>
2025-09-28 01:13:52 +0530

    Refactor authentication middleware to handle redirects for unauthenticated users and improve event access control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,50 @@
-export { auth as middleware } from "@/auth";
+export { auth } from "@/auth";
+
+import { NextResponse } from "next/server";
+import { auth } from "@/auth";
+
+export default auth((req) => {
+  const role = req.auth?.user?.role;
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith("/dashboard")) {
+    if (!req.auth) {
+      // user not logged in → redirect to login
+      return NextResponse.redirect(new URL("/login", req.url));
+      // logged in → allow
+    }
+  }
+
+  if (pathname.startsWith("/events")) {
+    const segments = pathname.split("/").filter(Boolean); // split path into ["events", "id", "groups"]
+
+    //for /events/new
+    if (segments[1] === "new") {
+      //redirect to /login for not logged-in users
+      if (!req.auth) return NextResponse.redirect(new URL("/login", req.url));
+
+      //redirect to forbidden page for logged-in users
+      if (role !== "club-admin") {
+        return NextResponse.redirect(new URL("/forbidden", req.url));
+      }
+    }
+    //no login for /events and /events/:id
+    if (segments.length <= 2) return NextResponse.next();
+
+    if (segments[2] === "attendance" || segments[2] === "assign-winner") {
+      if (!req.auth) return NextResponse.redirect(new URL("/login", req.url));
+      if (role !== "club-admin") {
+        return NextResponse.redirect(new URL("/forbidden", req.url));
+      }
+    } else {
+      // Other nested routes under /events/:id → just login required
+      if (!req.auth) return NextResponse.redirect(new URL("/login", req.url));
+    }
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ["/dashboard", "/events/:path*"],
+};
