76cb8aac57c935640b389407ad333b2167e3d636
sh66n <shaanedroos85@gmail.com>
2026-01-15 01:14:54 +0530

    feat: update Cancellations and Refunds policy for clarity and detail

--- src/app/(app)/(main)/cancellations-and-refunds/page.tsx
+++ src/app/(app)/(main)/cancellations-and-refunds/page.tsx
@@ -6,43 +6,28 @@ export default function CancellationsAndRefunds() {
       <h1 className="text-5xl font-semibold mb-8">Cancellations and Refunds</h1>
 
       <div>
-        For the purpose of these Terms and Conditions, The term "we", "us",
-        "our" used anywhere on this page shall mean SHAAN SAMEER EDROOS, whose
-        registered/operational office is Lok Udyan, Near Raheja Kalyan
-        MAHARASHTRA 421301 . "you", “your”, "user", “visitor” shall mean any
-        natural or legal person who is visiting our website and/or agreed to
-        purchase from us. Your use of the website and/or purchase from us are
-        governed by following Terms and Conditions: The content of the pages of
-        this website is subject to change without notice. Neither we nor any
-        third parties provide any warranty or guarantee as to the accuracy,
-        timeliness, performance, completeness or suitability of the information
-        and materials found or offered on this website for any particular
-        purpose. You acknowledge that such information and materials may contain
-        inaccuracies or errors and we expressly exclude liability for any such
-        inaccuracies or errors to the fullest extent permitted by law. Your use
-        of any information or materials on our website and/or product pages is
-        entirely at your own risk, for which we shall not be liable. It shall be
-        your own responsibility to ensure that any products, services or
-        information available through our website and/or product pages meet your
-        specific requirements. Our website contains material which is owned by
-        or licensed to us. This material includes, but are not limited to, the
-        design, layout, look, appearance and graphics. Reproduction is
-        prohibited other than in accordance with the copyright notice, which
-        forms part of these terms and conditions. All trademarks reproduced in
-        our website which are not the property of, or licensed to, the operator
-        are acknowledged on the website. Unauthorized use of information
-        provided by us shall give rise to a claim for damages and/or be a
-        criminal offense. From time to time our website may also include links
-        to other websites. These links are provided for your convenience to
-        provide further information. You may not create a link to our website
-        from another website or document without SHAAN SAMEER EDROOS’s prior
-        written consent. Any dispute arising out of use of our website and/or
-        purchase with us and/or any engagement with us is subject to the laws of
-        India . We, shall be under no liability whatsoever in respect of any
-        loss or damage arising directly or indirectly out of the decline of
-        authorization for any Transaction, on Account of the Cardholder having
-        exceeded the preset limit mutually agreed by us with our acquiring bank
-        from time to time
+        SHAAN SAMEER EDROOS believes in helping its customers as far as
+        possible, and has therefore a liberal cancellation policy. Under this
+        policy: Cancellations will be considered only if the request is made
+        within Not Applicable of placing the order. However, the cancellation
+        request may not be entertained if the orders have been communicated to
+        the vendors/merchants and they have initiated the process of shipping
+        them. SHAAN SAMEER EDROOS does not accept cancellation requests for
+        perishable items like flowers, eatables etc. However, refund/replacement
+        can be made if the customer establishes that the quality of product
+        delivered is not good. In case of receipt of damaged or defective items
+        please report the same to our Customer Service team. The request will,
+        however, be entertained once the merchant has checked and determined the
+        same at his own end. This should be reported within Not Applicable of
+        receipt of the products. In case you feel that the product received is
+        not as shown on the site or as per your expectations, you must bring it
+        to the notice of our customer service within Not Applicable of receiving
+        the product. The Customer Service Team after looking into your complaint
+        will take an appropriate decision. In case of complaints regarding
+        products that come with a warranty from manufacturers, please refer the
+        issue to them. In case of any Refunds approved by the SHAAN SAMEER
+        EDROOS, it’ll take Not Applicable for the refund to be processed to the
+        end customer.
       </div>
     </div>
   );
