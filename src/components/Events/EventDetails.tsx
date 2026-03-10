4ab50245c407211b9ff0019b3188b94892c31593
sh66n <shaanedroos85@gmail.com>
2026-03-10 15:43:41 +0530

    fix: fixed cta for free registrations

--- src/components/Events/EventDetails.tsx
+++ src/components/Events/EventDetails.tsx
@@ -341,7 +341,7 @@ export default function EventDetails({
                       ? "Registered"
                       : hasEventPassed
                         ? "Registrations Closed"
-                        : isLoading
+                        : registrationStatus === "processing"
                           ? "Registering..."
                           : "Register"}
                   </button>
